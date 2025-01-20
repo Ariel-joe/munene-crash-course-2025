56ac093d73a5ba98a116502a4ab9db168e28469d
Ariel-Joe <arieljoe00@gmail.com>
2025-01-20 17:06:53 +0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { Devs } from "./components/Devs";
+import { BrowseJobs } from "./components/BrowseJobs";
+import { ViewAllJobs } from "./components/ViewAllJobs";
 
 const App = () => {
   return (
@@ -13,16 +15,12 @@ const App = () => {
       < Devs />
 
       {/* <!-- Browse Jobs --> */}
-      
+      < BrowseJobs />
 
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      {/* view all jobs */}
+      <ViewAllJobs />
+
+      
     </>
   );
 };
